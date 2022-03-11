b99aeb38d5038110818f1d244f32f66bff82dadf
IJustDev <contact@alexander-panov.de>
2022-03-11 22:51:17 +0200

    fix: rules for accessing locked content

--- app/lib/content_restrictor.rb
+++ app/lib/content_restrictor.rb
@@ -6,15 +6,7 @@ class ContentRestrictor
   include Singleton
 
   def unlocked_for?(status, account)
-    found_purchase = StatusPurchase.where(status_id: status.id, account: account)
-    status.cost.nil? or !found_purchase.empty?
-  end
-
-  def purchase_text(status)
-    "This status is locked. Click #{link_to_purchase 'here', status} to purchase it for $ #{status.cost}."
-  end
-
-  def link_to_purchase(display_text, status)
-    "https://example.com"
+    found_purchase = StatusPurchase.find_by(status_id: status.id, account: account, state: :succeed)
+    status.cost.nil? or !found_purchase.nil? or status.account == account
   end
 end
