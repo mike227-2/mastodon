8d9365da9b8147eb4ed232a1d1e75f3ce9fe1a08
IJustDev <contact@alexander-panov.de>
2022-03-15 11:01:57 +0200

    refactor: move filter locked to content restrictor

--- app/lib/content_restrictor.rb
+++ app/lib/content_restrictor.rb
@@ -9,4 +9,23 @@ class ContentRestrictor
     found_purchase = StatusPurchase.find_by(status_id: status.id, account: account, state: :succeed)
     status.cost.nil? or !found_purchase.nil? or status.account == account
   end
+
+  def filter_locked_statuses(statuses, account)
+    all_statuses = []
+
+    statuses.each do |status|
+      all_statuses.push(filter_locked_status(status, account))
+    end
+    all_statuses
+  end
+
+  def filter_locked_status(status, account)
+    unless status.unlocked_for? account
+      status.locked = true
+      status.media_attachments_count = status.media_attachments.length
+      status.media_attachments = []
+      status.text = 'Locked'
+    end
+    status
+  end
 end
